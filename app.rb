ccf8c25e8d1ba3ea8020d68003225ac37d0e7c1b
katsapvlad <katsapvlad@yandex.by>
2020-11-11 18:07:30 +0300

    Keep values on post

--- app.rb
+++ app.rb
@@ -29,18 +29,19 @@ end
 
 get '/visit' do
 	@barbers = Barber.order "created_at DESC"
+	@c = Client.new
 	erb :visit
 end
 
 post '/visit' do
 
-	c = Client.new params[:client]
-	c.save
+	@c = Client.new params[:client]
+	@c.save
 	
-	if c.save
+	if @c.save
 		erb "<h2>Вы записались!</h2>"
 	else
-		@error = c.errors.full_messages.first
+		@error = @c.errors.full_messages.first
 		erb :visit
 	end
 	
